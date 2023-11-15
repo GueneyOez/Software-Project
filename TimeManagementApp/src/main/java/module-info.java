83f8524d56de4cb47b9d254c76b52aadcd1c2b9b
Güney Özcan <148807239+GueneyOez@users.noreply.github.com>
2023-11-16 00:08:50 +0100

    Delete TimeManagementApp directory

--- TimeManagementApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.timemanagementapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.timemanagementapp to javafx.fxml;
-    exports com.example.timemanagementapp;
-}
