1a061f2698acebb76e5cba0713482efaaa9dc5e1
Gisnack <simon.peiskar@gmail.com>
2023-11-18 15:20:21 +0100

    Login Screen changed to .fxml

---
+++ TimeManagementApp/src/main/java/controller/LoginController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Button loginButton1;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
